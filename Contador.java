99951e840ccb66beafcfafb08459fe4530319331
HeleOficial <juanledesmaidarraga@gmail.com>
2025-02-22 00:30:16 +0100

    Update Contador.java

--- Contador.java
+++ Contador.java
@@ -1,28 +1,20 @@
- import java.util.Scanner;
- 
- public class Contador {
-     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         
-         System.out.print("Ingrese una palabra en minúsculas: ");
-         String palabra = scanner.nextLine();
-         
-         int vocales = 0, consonantes = 0;
- 
-         String vocalesStr = "aeiou";
- 
-         for (char letra : palabra.toCharArray()) {
-             if (vocalesStr.indexOf(letra) != -1) {
-                 vocales++;
-             } else {
-                 consonantes++;
-             }
-         }
- 
-         System.out.println("Número de vocales: " + vocales);
-         System.out.println("Número de consonantes: " + consonantes);
- 
-         scanner.close();
-     }
- }
- 
+public class contador {
+
+    public static void main(String[] args) {
+        String texto = "Hola mundo, este es un texto de prueba.";
+        int contadorVocales = 0;
+        int contadorConsonantes = 0;
+        for (char caracter : texto.toCharArray()) {
+            if (Character.isLetter(caracter)) {
+                caracter = Character.toLowerCase(caracter);
+                if ("aeiou".indexOf(caracter) != -1) {
+                    contadorVocales++;
+                } else {
+                    contadorConsonantes++;
+                }
+            }
+        }
+        System.out.println("Número de vocales: " + contadorVocales);
+        System.out.println("Número de consonantes: " + contadorConsonantes);
+    }
+}
