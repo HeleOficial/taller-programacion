c94bf34bc59c5d9db22e821f49e4570b3c109b6d
HeleOficial <juanledesmaidarraga@gmail.com>
2025-02-22 00:20:23 +0100

    Update InvertirCadena.java

--- InvertirCadena.java
+++ InvertirCadena.java
@@ -4,14 +4,11 @@ public class InvertirCadena {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Solicitar la cadena al usuario
         System.out.print("Ingrese una cadena de texto: ");
         String texto = scanner.nextLine();
         
-        // Invertir la cadena usando un StringBuilder
         String textoInvertido = new StringBuilder(texto).reverse().toString();
 
-        // Mostrar la cadena invertida
         System.out.println("Cadena invertida: " + textoInvertido);
         
         scanner.close();
