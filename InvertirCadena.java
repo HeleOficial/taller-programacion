34e60108bd67139a0fc7117596bd991ff014c3c6
HeleOficial <juanledesmaidarraga@gmail.com>
2025-02-22 01:05:45 +0100

    Update InvertirCadena.java

--- InvertirCadena.java
+++ InvertirCadena.java
@@ -1,6 +1,6 @@
 public class InvertiCadena {
     public static void main(String[] args) {
-        String cadena = "Hola Mundo";
+        String cadena = "Pin pom es un muñeco muy guapo y de carton";
         String cadenaInvertida = new StringBuilder(cadena).reverse().toString();
         System.out.println("Cadena original: " + cadena);
         System.out.println("Cadena invertida: " + cadenaInvertida);
